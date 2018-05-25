42ea97f190f10654f82351695b2e383b810c04f7
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-05-25 08:33:32 -0500

    server up and running

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,46 @@
 console.log('in client.js');
+
+$( document ).ready( readyNow ); 
+
+function readyNow() {
+    console.log( 'in client.js' );
+    getQuote(); 
+}//end readyNow
+
+function addQuote() {
+    console.log( 'in addQuote' );
+    let objectToSend = {
+        text: $( '#text' ).val()
+        author: $( '#author' ).val()
+    }; // end objectToSend
+    console.log('sending:', objectToSend );
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        data: objectToSend
+    }).then( function( response ){
+        console.log('back from server with:', response );
+    }); 
+    getQuote();  
+}
+
+function displayQuote( quotes ){
+    let el = $( '#quotesOut' );
+    el.empty();
+    for( quote of quotes ){
+        el.append( '<div>' + quote.name + '</div>')
+    }
+}
+
+function getQuote() {
+    console.log('in getQuote');
+    //make AJAX GET call to server to get quotes
+    $.ajax({
+        method: 'GET',
+        url: '/quotes',
+        //once AJAX is doen, then run this function for a response
+    }).then( function( response ){
+        console.log('back from server with:', response );
+        displayQuote( response );
+    }); // end AJAX
+}// end getQuotes
--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 // requires
-let express = require( 'express' );
-let app = express();
-let bodyParser = require( 'body-parser' );
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
 // global variables
 const port = 5000;
-let guests = [];
+// let name = 'Shannon'; 
+// let body = 'I like ice cream';
+let quotes = [{
+    text: 'I love ice cream', 
+    author: 'Shannon Engstrom'
+} ];
 
 // app.uses
 app.use( express.static( 'server/public' ) );
@@ -15,14 +20,14 @@ app.listen( port, ()=>{
 }); //end server up
 
 // get route
-app.get( '/guests', ( req, res )=>{
-    console.log( 'in GET hit for /guests route' );
-    res.send( guests );
-}); //end guests GET
+app.get( '/quotes', ( req, res )=>{
+    console.log( 'in GET hit for /quotes route', req.body );
+    res.send( quotes );
+}); //end quotes GET
 
 // post route - establishes new route on URL
-app.post( '/guests', (req, res)=>{
-    console.log( 'in POST hit for /guests route:', req.body );
-    guests.push( req.body.name );
+app.post( '/quotes', (req, res)=>{
+    console.log( 'in POST hit for /quotes route:', req.body );
+   quotes.push( req.body );
     res.sendStatus( 200 );
-}); // end guests POST
+}); // end quotes POST
